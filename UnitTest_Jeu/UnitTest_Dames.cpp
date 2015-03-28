1c2ed437b784888cb11b5e050dc6fbc4a632f585
kimsavinfo <kimsavinfo@gmail.com>
2015-03-28 19:08:44 +0100

    Dames : test poser un pion

--- UnitTest_Jeu/UnitTest_Dames.cpp
+++ UnitTest_Jeu/UnitTest_Dames.cpp
@@ -2,6 +2,7 @@
 #include "CppUnitTest.h"
 
 #include "DriverGrilleDames.h"
+#include "JoueurDames.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -9,18 +10,33 @@ namespace UnitTest_Jeu
 {
 	static int nbColonnes = 6;
 	static int nbLignes = 6;
+	static JoueurDames * joueurA;
+	static JoueurDames * joueurB;
 
 	TEST_CLASS(UnitTest_Dames)
 	{
 		public:
 		
-			TEST_METHOD(Dames_initGrille)
+			TEST_CLASS_INITIALIZE(ClassInitialize) 
 			{
-				DriverGrilleDames * driver  = new DriverGrilleDames(nbLignes, nbColonnes);
-
-				Assert::AreEqual(nbLignes , driver->getGrilleNbLignes());
-				Assert::AreEqual(nbColonnes , driver->getGrilleNbColonnes());
+				joueurA = new JoueurDames("Alice", "W");
+				joueurB = new JoueurDames("Bob", "B");
 			}
 
+			TEST_CLASS_CLEANUP(ClassCleanup)
+			{
+				delete joueurA;
+				delete joueurB;
+			}
+
+			TEST_METHOD(Dames_PoserPion)
+			{
+				DriverGrilleDames * driver  = new DriverGrilleDames(nbLignes, nbColonnes);
+				vector<long> pionsIds = joueurA->getPionsIds();
+
+				driver->poserPion(pionsIds[0], 0, 2);
+
+				Assert::AreEqual(pionsIds[0], driver->getCaseIdOccupant(0,2));
+			}
 	};
 }
